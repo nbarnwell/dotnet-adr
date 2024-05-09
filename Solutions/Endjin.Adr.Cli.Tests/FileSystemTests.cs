9e6f1dc8a4184f5323a1d563985d83bfd4bf9af8
Neil Barnwell <neil.barnwell@bybox.com>
2024-05-09 18:40:03 +0100

    Use Spectre.IO to abstract current working directory

--- Solutions/Endjin.Adr.Cli.Tests/FileSystemTests.cs
+++ Solutions/Endjin.Adr.Cli.Tests/FileSystemTests.cs
@@ -1,24 +1,35 @@
 ﻿using Endjin.Adr.Cli.Abstractions;
 using NUnit.Framework;
+using Spectre.IO;
+using Spectre.IO.Testing;
+using Environment = System.Environment;
 
 namespace Endjin.Adr.Cli.Tests;
 
 [TestFixture]
 public class FileSystemTests
 {
+    private IEnvironment environment;
+
+    [SetUp]
+    public void SetUp()
+    {
+        this.environment = FakeEnvironment.CreateWindowsEnvironment();
+    }
+
     [Test]
     public void Get_current_working_directory()
     {
         Assert.That(WorkingDirectory.Current, Is.EqualTo(Environment.CurrentDirectory));
 
-        WorkingDirectory.SetCurrentDirectory(() => @"C:\Temp");
-        Assert.That(WorkingDirectory.Current, Is.EqualTo(@"C:\Temp"));
+        this.environment.SetWorkingDirectory(DirectoryPath.FromString(@"C:\Temp"));
+        WorkingDirectory.SetCurrentDirectory(() => this.environment.WorkingDirectory.FullPath);
+        Assert.That(WorkingDirectory.Current, Is.EqualTo(@"C:/Temp"));
     }
 
     [Test]
     public void Find_repository_root()
     {
-        //IRepositoryRootLocator locator = new ();
         
     }
 }
